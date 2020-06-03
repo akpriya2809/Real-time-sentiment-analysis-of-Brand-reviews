0075f84eeb7bfde9b9109e6bbe16b8ae0aade318
Akanksha <akanksha.priya20@gmail.com>
2020-06-02 23:05:08 -0700

    torch chnages

--- training/train.py
+++ training/train.py
@@ -358,20 +358,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         'Character Based CNN for text classification')
     parser.add_argument('--data_path', type=str,
-                        default='./data/train.csv')
-    parser.add_argument('--validation_split', type=float, default=0.2)
-    parser.add_argument('--label_column', type=str, default='Sentiment')
-    parser.add_argument('--text_column', type=str, default='SentimentText')
+                        default='/Users/akankshapriya/PycharmProjects/Real-Time-Sentiment-Analysis/Real-Time-sentiment-analysis-of-Brand-reviews/scraping/scrapy/trustpilot/comments_trustpilot_en.csv')
+    parser.add_argument('--validation_split', type=float, default=0.1)
+    parser.add_argument('--label_column', type=str, default='rating')
+    parser.add_argument('--text_column', type=str, default='comment')
     parser.add_argument('--max_rows', type=int, default=None)
     parser.add_argument('--chunksize', type=int, default=50000)
     parser.add_argument('--encoding', type=str, default='utf-8')
     parser.add_argument('--sep', type=str, default=',')
     parser.add_argument('--steps', nargs='+', default=['lower'])
     parser.add_argument('--group_labels', type=int, default=1, choices=[0, 1])
-    parser.add_argument('--ignore_center', type=int, default=1, choices=[0, 1])
+    parser.add_argument('--ignore_center', type=int, default=0, choices=[0, 1])
     parser.add_argument('--label_ignored', type=int, default=None)
     parser.add_argument('--ratio', type=float, default=1)
-    parser.add_argument('--balance', type=int, default=0, choices=[0, 1])
+    parser.add_argument('--balance', type=int, default=1, choices=[0, 1])
     parser.add_argument('--use_sampler', type=int,
                         default=0, choices=[0, 1])
 
@@ -379,8 +379,8 @@ if __name__ == "__main__":
                         default="abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+ =<>()[]{}")
     parser.add_argument('--number_of_characters', type=int, default=69)
     parser.add_argument('--extra_characters', type=str, default='')
-    parser.add_argument('--max_length', type=int, default=150)
-    parser.add_argument('--dropout_input', type=float, default=0.1)
+    parser.add_argument('--max_length', type=int, default=1014)
+    parser.add_argument('--dropout_input', type=float, default=0)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--optimizer', type=str,
@@ -404,12 +404,12 @@ if __name__ == "__main__":
                         choices=[0, 1], default=1)
     parser.add_argument('--workers', type=int, default=1)
     parser.add_argument('--log_path', type=str, default='./logs/')
-    parser.add_argument('--log_every', type=int, default=100)
+    parser.add_argument('--log_every', type=int, default=250)
     parser.add_argument('--log_f1', type=int, default=1, choices=[0, 1])
     parser.add_argument('--flush_history', type=int,
                         default=1, choices=[0, 1])
     parser.add_argument('--output', type=str, default='./models/')
-    parser.add_argument('--model_name', type=str, default='')
+    parser.add_argument('--model_name', type=str, default='en_trustpilot')
 
     args = parser.parse_args()
     run(args)
